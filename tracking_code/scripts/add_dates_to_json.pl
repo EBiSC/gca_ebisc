f26457dd0f98e8e6d4ed00f7dc99394c4038377a
istreeter <streeter@ebi.ac.uk>
2015-08-28 17:04:37 +0100

    script to add date of error to the json file

---
+++ tracking_code/scripts/add_dates_to_json.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use JSON qw();
+use List::Util qw();
+
+my ($new_api_compares_json, $old_api_compares_json);
+
+GetOptions("new_api_compares_json=s" => \$new_api_compares_json,
+        "old_api_compares_json=s" => \$old_api_compares_json,
+);
+
+my ($IN, @lines);
+open $IN, '<', $new_api_compares_json or die "could not open $new_api_compares_json $!";
+@lines = <$IN>;
+close $IN;
+my $new_api_compares = JSON::decode_json(join('', @lines));
+close $IN;
+
+open $IN, '<', $old_api_compares_json or die "could not open $old_api_compares_json $!";
+@lines = <$IN>;
+close $IN;
+my $old_api_compares = JSON::decode_json(join('', @lines));
+close $IN;
+
+my $date = $new_api_compares->{'date'} or die "could not extract date";
+$date =~ s/ \d\d:\d\d:\d\d//;
+
+CELL_LINE:
+foreach my $cell_line (@{$new_api_compares->{'lines'}}) {
+  my $old_cell_line = List::Util::first {$_->{'consensus'}{'name'}{'val'} eq $cell_line->{'consensus'}{'name'}{'val'}} @{$old_api_compares->{'lines'}};
+  $old_cell_line ||= List::Util::first {$_->{'consensus'}{'biosample_id'}{'val'} eq $cell_line->{'consensus'}{'biosample_id'}{'val'}} @{$old_api_compares->{'lines'}};
+
+  add_date($cell_line->{'IMS'}{'biosample_id'}, $old_cell_line->{'IMS'}{'biosample_id'});
+  add_date($cell_line->{'IMS'}{'donor_biosample_id'}, $old_cell_line->{'IMS'}{'donor_biosample_id'});
+  add_date($cell_line->{'IMS'}{'name'}, $old_cell_line->{'IMS'}{'name'});
+  add_date($cell_line->{'biosample'}{'id'}, $old_cell_line->{'biosample'}{'id'});
+  add_date($cell_line->{'consensus'}{'biosample_id'}, $old_cell_line->{'consensus'}{'biosample_id'});
+  add_date($cell_line->{'consensus'}{'donor_biosample'}, $old_cell_line->{'consensus'}{'donor_biosample_id'});
+  add_date($cell_line->{'consensus'}{'name'}, $old_cell_line->{'consensus'}{'name'});
+  add_date($cell_line->{'hESCreg'}{'biosample_id'}, $old_cell_line->{'hESCreg'}{'biosample_id'});
+  add_date($cell_line->{'hESCreg'}{'donor_biosample_id'}, $old_cell_line->{'hESCreg'}{'donor_biosample_id'});
+  add_date($cell_line->{'hESCreg'}{'name'}, $old_cell_line->{'hESCreg'}{'name'});
+};
+
+print JSON::encode_json($new_api_compares);
+
+sub add_date {
+  my ($new_data, $old_data) = @_;
+  return if !$new_data;
+  return if !$new_data->{'error'};
+  $old_data //= {};
+  $new_data->{'error_date'} = $old_data->{'error_date'} || $date;
+}
--- tracking_code/shell/run_tracking.bash
+++ tracking_code/shell/run_tracking.bash
@@ -4,7 +4,8 @@ VERSION=`date "+%Y%m%d.%H%M%S"`
 TRACK_HOME=/nfs/production/reseq-info/work/ebiscdcc/tracking_json
 mkdir $TRACK_HOME/$VERSION
 
-perl $EBISC_CODE/tracking_code/scripts/create_tracking_json.pl -hESCreg_user="$HESCREG_USER" -hESCreg_pass="$HESCREG_PASS" -IMS_user="$IMS_USER" -IMS_pass="$IMS_PASS" >$TRACK_HOME/$VERSION/api_compares.$VERSION.json \
+perl $EBISC_CODE/tracking_code/scripts/create_tracking_json.pl -hESCreg_user="$HESCREG_USER" -hESCreg_pass="$HESCREG_PASS" -IMS_user="$IMS_USER" -IMS_pass="$IMS_PASS" >$TRACK_HOME/$VERSION/api_compares.no_dates.$VERSION.json \
+&& perl $EBISC_CODE/tracking_code/scripts/add_dates_to_json.pl -new_api_compares_json $TRACK_HOME/$VERSION/api_compares.no_dates.$VERSION.json -old_api_compares_json $TRACK_HOME/api_compares.current.json > $TRACK_HOME/$VERSION/api_errors.$VERSION.json \
 && perl $EBISC_CODE/tracking_code/scripts/create_error_json.pl -api_compares_json $TRACK_HOME/$VERSION/api_compares.$VERSION.json > $TRACK_HOME/$VERSION/api_errors.$VERSION.json \
 && perl $EBISC_CODE/tracking_code/scripts/create_tests_json.pl -api_compares_json $TRACK_HOME/$VERSION/api_compares.$VERSION.json > $TRACK_HOME/$VERSION/api_tests.$VERSION.json \
 && cp $TRACK_HOME/$VERSION/api_compares.$VERSION.json /homes/ebiscdcc/public_html/track/json/api_compares.json \
@@ -12,4 +13,5 @@ perl $EBISC_CODE/tracking_code/scripts/create_tracking_json.pl -hESCreg_user="$H
 && cp $TRACK_HOME/$VERSION/api_tests.$VERSION.json /homes/ebiscdcc/public_html/track/json/tests.json \
 && ln -sT $TRACK_HOME/$VERSION/api_compares.$VERSION.json $TRACK_HOME/api_compares.current.json \
 && ln -sT $TRACK_HOME/$VERSION/api_errors.$VERSION.json $TRACK_HOME/api_errors.current.json \
-&& ln -sT $TRACK_HOME/$VERSION/api_tests.$VERSION.json $TRACK_HOME/api_tests.current.json
+&& ln -sT $TRACK_HOME/$VERSION/api_tests.$VERSION.json $TRACK_HOME/api_tests.current.json \
+&& rm -f $TRACK_HOME/$VERSION/api_compares.no_dates.$VERSION.json
