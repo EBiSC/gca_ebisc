1f666e883d8be61ecab0c92d1b49920f23c79797
peterwharrison <peter@ebi.ac.uk>
2015-09-04 15:14:50 +0100

    Fix: Sorting by column groups failed and cannot test seperately and always has failed at top

--- progress_webapp/app/js.20150828/controllers.js
+++ progress_webapp/app/js.20150828/controllers.js
@@ -17,15 +17,14 @@ controllers.controller('LineListCtrl', ['List', function(List) {
       this.data.lines.sort(
         column === 'IMSExported' ? function(a,b) {return b.IMS.exported.error - a.IMS.exported.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
           : column === 'hESCregExported' ? function(a,b) {return b.hESCreg.exported.error - a.hESCreg.exported.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
-          : column === 'hESCregValidated' ? function(a,b) {return b.hESCreg.exported.error - a.hESCreg.exported.error || b.hESCreg.validated.error - a.hESCreg.validated.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
+          : column === 'hESCregValidated' ? function(a,b) {return b.hESCreg.validated.error - a.hESCreg.validated.error || a.hESCreg.exported.error - b.hESCreg.exported.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
           : column === 'biosampleExported' ? function(a,b) {return b.biosample.exported.error - a.biosample.exported.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
           : column === 'alternateNames' ? function(a,b) {return a.consensus.alternate_names.localeCompare(b.consensus.alternate_names) || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
-          : column === 'IMSName' ? function(a,b) {return a.IMS.name.error - b.IMS.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
-          : column === 'IMSBiosample' ? function(a,b) {return a.IMS.biosample_id.error - b.IMS.biosample_id.error || a.consensus.biosample_id.val.localeCompare(b.consensus.biosample_id.val)}
-          : column === 'hESCregName' ? function(a,b) {return a.hESCreg.name.error - b.hESCreg.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
-          : column === 'hESCregBiosample' ? function(a,b) {return a.hESCreg.biosample_id.error - b.hESCreg.biosample_id.error || a.consensus.biosample_id.val.localeCompare(b.consensus.biosample_id.val)}
-          : column === 'biosampleVials' ? function(a,b) {return (a.biosample.vial_count || 0) - (b.biosample.vial_count || 0) || a.consensus.name.val.localeCompare(b.consensus.name.val)}
-          : column === 'donorBiosampleError' ? function(a,b) {return a.consensus.donor_biosample.error - b.consensus.donor_biosample.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
+          : column === 'IMSName' ? function(a,b) {return b.IMS.name.error - a.IMS.name.error || a.IMS.exported.error - b.IMS.exported.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
+          : column === 'IMSBiosample' ? function(a,b) {return b.IMS.biosample_id.error - a.IMS.biosample_id.error || a.IMS.exported.error - b.IMS.exported.error || a.consensus.biosample_id.val.localeCompare(b.consensus.biosample_id.val)}
+          : column === 'hESCregName' ? function(a,b) {return b.hESCreg.name.error - a.hESCreg.name.error || a.hESCreg.exported.error - b.hESCreg.exported.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
+          : column === 'hESCregBiosample' ? function(a,b) {return b.hESCreg.biosample_id.error - a.hESCreg.biosample_id.error || a.hESCreg.exported.error - b.hESCreg.exported.error || a.hESCreg.validated.error - b.hESCreg.validated.error || a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
+          : column === 'donorBiosampleError' ? function(a,b) {return b.consensus.donor_biosample.error - a.consensus.donor_biosample.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
           : function(a,b) {return a.consensus.name.error - b.consensus.name.error || a.consensus.name.val.localeCompare(b.consensus.name.val)}
       );
       if (column === this.sortColumn) {
