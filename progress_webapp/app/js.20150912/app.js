ea74428248e91558bdf20fe59556eacc141987f0
istreeter <streeter@ebi.ac.uk>
2015-09-12 23:18:26 +0100

    renaming js and partials ready for a release

--- progress_webapp/app/js.20150912/app.js
+++ progress_webapp/app/js.20150912/app.js
@@ -15,22 +15,22 @@ ebiscProgress.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
     when('/', {
-      templateUrl: 'partials.20150907/line-list.html',
+      templateUrl: 'partials.20150912/line-list.html',
       controller: 'LineListCtrl',
       controllerAs: 'LineCtrl',
     }).
     when('/errors', {
-      templateUrl: 'partials.20150907/errors.html',
+      templateUrl: 'partials.20150912/errors.html',
       controller: 'ErrorListCtrl',
       controllerAs: 'ErrorCtrl',
     }).
     when('/tests', {
-      templateUrl: 'partials.20150907/tests.html',
+      templateUrl: 'partials.20150912/tests.html',
       controller: 'TestListCtrl',
       controllerAs: 'TestCtrl',
     }).
     when('/:line', {
-      templateUrl: 'partials.20150907/line-detail.html',
+      templateUrl: 'partials.20150912/line-detail.html',
       controller: 'LineDetailCtrl',
       controllerAs: 'LineCtrl',
     }).
