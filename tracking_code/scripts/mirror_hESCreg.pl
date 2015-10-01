9fdb73fd6853154bf09805b11f4793605da36bc3
istreeter <streeter@ebi.ac.uk>
2015-10-01 17:55:29 +0100

    script to mirror hESCreg into an elasticsearch index

---
+++ tracking_code/scripts/mirror_hESCreg.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+use ReseqTrack::EBiSC::hESCreg;
+use Search::Elasticsearch;
+use utf8;
+
+my ($hESCreg_user, $hESCreg_pass);
+my $es_host='vg-rs-dev1:9200';
+
+GetOptions("user=s" => \$hESCreg_user,
+    "pass=s" => \$hESCreg_pass,
+    'es_host=s' =>\$es_host,
+);
+die "missing credentials" if !$hESCreg_user || !$hESCreg_pass;
+my $elasticsearch = Search::Elasticsearch->new(nodes => $es_host);
+
+$elasticsearch->indices->delete(index => 'hescreg');
+$elasticsearch->indices->create(index => 'hescreg');
+$elasticsearch->indices->put_mapping(
+  index => 'hescreg',
+  type => 'line',
+  body => {
+    line => {
+      properties => {
+        id => {type => 'integer'},
+        name => {type => 'string', index => 'not_analyzed'},
+        alternate_name => {type => 'string', index => 'not_analyzed'},
+        biosamples_id => {type => 'string', index => 'not_analyzed'},
+        donor_biosamples_id => {type => 'string', index => 'not_analyzed'},
+      }
+    }
+  }
+);
+
+my $hESCreg = ReseqTrack::EBiSC::hESCreg->new(
+  user => $hESCreg_user,
+  pass => $hESCreg_pass,
+);
+
+LINE:
+foreach my $id (1..2000) {
+  my $line = eval{$hESCreg->get_line($id);};
+  next LINE if !$line || $@;
+  $elasticsearch->index(
+    index => 'hescreg',
+    type => 'line',
+    id => $id,
+    body => $line,
+    );
+}
