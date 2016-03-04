0705010ccb748769089c37c1ed3eda90b8583e30
peterwharrison <peter@ebi.ac.uk>
2016-03-04 13:40:14 +0000

    Updates to field names to reflect IMS API names

--- tracking_code/lib/ReseqTrack/EBiSC/IMS.pm
+++ tracking_code/lib/ReseqTrack/EBiSC/IMS.pm
@@ -60,45 +60,45 @@ sub subset_lims_fields {
     if ($$sample{donor}{gender}){$sample_index->{donor}{gender} = $$sample{donor}{gender};}
     if ($$sample{donor}{biosamples_id}){$sample_index->{donor}{biosamples_id} = $$sample{donor}{biosamples_id};}
     if ($$sample{donor}{internal_donor_ids}){$sample_index->{donor}{internal_donor_ids} = $$sample{donor}{internal_donor_ids};}
-    if ($$sample{donor}{disease_associated_phenotypes}){$sample_index->{donor}{disease_associated_phenotypes} = $$sample{donor}{disease_associated_phenotypes};}
     if ($$sample{donor}{phenotypes}){$sample_index->{donor}{phenotypes} = $$sample{donor}{phenotypes};}
-    if ($$sample{donor}{karyotype}){$sample_index->{donor}{karyotype} = $$sample{donor}{karyotype};}
+    if ($$sample{donor}{karyotype}){$sample_index->{donor}{karyotype} = $$sample{donor}{karyotype};}  #TODO No data yet needs to be tested when data is availible
 
-    if ($$sample{cellline_karyotype}){$sample_index->{cellline_karyotype} = $$sample{cellline_karyotype}{karyotype};}
+    if ($$sample{cellline_karyotype}{karyotype}){$sample_index->{cellline_karyotype} = $$sample{cellline_karyotype}{karyotype};}
 
+    if ($$sample{reprogramming_method_vector_free_types}){$sample_index->{reprogramming_method_vector_free_types} = $$sample{reprogramming_method_vector_free_types};}
     if ($$sample{reprogramming_method}{type}){$sample_index->{reprogramming_method}{type} = $$sample{reprogramming_method}{type};}
-    if ($$sample{reprogramming_method_vector_free_types}){$sample_index->{reprogramming_method}{data}{vector_free_types} = $$sample{reprogramming_method}{data}{vector_free_types};}
     if ($$sample{reprogramming_method}{data}{vector}){$sample_index->{reprogramming_method}{data}{vector} = $$sample{reprogramming_method}{data}{vector};}
     if ($$sample{reprogramming_method}{data}{virus}){$sample_index->{reprogramming_method}{data}{virus} = $$sample{reprogramming_method}{data}{virus};}
     if ($$sample{reprogramming_method}{data}{transposon}){$sample_index->{reprogramming_method}{data}{transposon} = $$sample{reprogramming_method}{data}{transposon};}
     if ($$sample{reprogramming_method}{data}{non_integrating_vector_gene_list}){$sample_index->{reprogramming_method}{data}{non_integrating_vector_gene_list} = $$sample{reprogramming_method}{data}{non_integrating_vector_gene_list};}
+    if ($$sample{reprogramming_method}{data}{non_integrating_vector_detectable}){$sample_index->{reprogramming_method}{data}{non_integrating_vector_detectable} = $$sample{reprogramming_method}{data}{non_integrating_vector_detectable};}
+    if ($$sample{reprogramming_method}{data}{non_integrating_vector_methods}){$sample_index->{reprogramming_method}{data}{non_integrating_vector_method} = $$sample{reprogramming_method}{data}{non_integrating_vector_method};}  #TODO No data yet needs to be tested when data is availible
+    if ($$sample{reprogramming_method}{data}{non_integrating_vector_detection_notes}){$sample_index->{reprogramming_method}{data}{non_integrating_vector_detection_notes} = $$sample{reprogramming_method}{data}{non_integrating_vector_detection_notes};}  #TODO No data yet needs to be tested when data is availible
     if ($$sample{reprogramming_method}{data}{integrating_vector_gene_list}){$sample_index->{reprogramming_method}{data}{integrating_vector_gene_list} = $$sample{reprogramming_method}{data}{integrating_vector_gene_list};}
-    if ($$sample{reprogramming_method}{data}{non_integrating_detectable}){$sample_index->{reprogramming_method}{data}{non_integrating_detectable} = $$sample{reprogramming_method}{data}{non_integrating_detectable};}
-    if ($$sample{reprogramming_method}{data}{non_integrating_methods}){$sample_index->{reprogramming_method}{data}{non_integrating_method} = $$sample{reprogramming_method}{data}{non_integrating_method};}
-    if ($$sample{reprogramming_method}{data}{non_integrating_detection_notes}){$sample_index->{reprogramming_method}{data}{non_integrating_detection_notes} = $$sample{reprogramming_method}{data}{non_integrating_detection_notes};}
     if ($$sample{reprogramming_method}{data}{integrating_vector_silenced}){$sample_index->{reprogramming_method}{data}{integrating_vector_silenced} = $$sample{reprogramming_method}{data}{integrating_vector_silenced};}
-    if ($$sample{reprogramming_method}{data}{integrating_vector_methods}){$sample_index->{reprogramming_method}{data}{integrating_vector_methods} = $$sample{reprogramming_method}{data}{integrating_vector_methods};}
-    if ($$sample{reprogramming_method}{data}{integrating_silencing_notes}){$sample_index->{reprogramming_method}{data}{integrating_vector_silencing_notes} = $$sample{reprogramming_method}{data}{integrating_vector_silencing_notes};}
-
-
-    if ($$sample{primary_disease_diagnosed}){$sample_index->{primary_disease_diagnosed} = $$sample{primary_disease_diagnosed};}
-    if ($$sample{primary_disease}{name}){$sample_index->{primary_disease}{name} = $$sample{primary_disease}{name};}
+    if ($$sample{reprogramming_method}{data}{integrating_vector_methods}){$sample_index->{reprogramming_method}{data}{integrating_vector_methods} = $$sample{reprogramming_method}{data}{integrating_vector_methods};}  #TODO No data yet needs to be tested when data is availible
+    if ($$sample{reprogramming_method}{data}{integrating_silencing_notes}){$sample_index->{reprogramming_method}{data}{integrating_vector_silencing_notes} = $$sample{reprogramming_method}{data}{integrating_vector_silencing_notes};}  #TODO No data yet needs to be tested when data is availible
+    
+    if ($$sample{primary_disease_diagnosed}){$sample_index->{disease_diagnosed} = $$sample{primary_disease_diagnosed};}
+    #FIXME when availible if ($$sample{disease_diagnosed}){$sample_index->{disease_diagnosed} = $$sample{disease_diagnosed};}
+    if ($$sample{primary_disease}{name}){$sample_index->{disease_names} = [$$sample{primary_disease}{name}];}
+    #FIXME when availible if ($$sample{disease_names}){$sample_index->{disease_names} = $$sample{disease_names};}
     if ($$sample{disease_associated_phenotypes}){$sample_index->{disease_associated_phenotypes} = $$sample{disease_associated_phenotypes};}
     
-    if ($$sample{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag}){$sample_index->{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag} = $$sample{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag};}
-    if ($$sample{cellline_disease_associated_genotype}{variant_of_interest_flag}){$sample_index->{cellline_disease_associated_genotype}{variant_of_interest_flag} = $$sample{cellline_disease_associated_genotype}{variant_of_interest_flag};}
-
     if ($$sample{depositor_cellline_culture_conditions}{surface_coating}){$sample_index->{depositor_cellline_culture_conditions}{surface_coating} = $$sample{depositor_cellline_culture_conditions}{surface_coating};}
-    if ($$sample{depositor_cellline_culture_conditions}{passage_method}){$sample_index->{depositor_cellline_culture_conditions}{passage_method} = $$sample{depositor_cellline_culture_conditions}{passage_method};}
-    if ($$sample{depositor_cellline_culture_conditions}{o2_concentration}){$sample_index->{depositor_cellline_culture_conditions}{o2_concentration} = $$sample{depositor_cellline_culture_conditions}{o2_concentration};}
     if ($$sample{depositor_cellline_culture_conditions}{co2_concentration}){$sample_index->{depositor_cellline_culture_conditions}{co2_concentration} = $$sample{depositor_cellline_culture_conditions}{co2_concentration};}
+    if ($$sample{depositor_cellline_culture_conditions}{o2_concentration}){$sample_index->{depositor_cellline_culture_conditions}{o2_concentration} = $$sample{depositor_cellline_culture_conditions}{o2_concentration};}
+    if ($$sample{depositor_cellline_culture_conditions}{passage_method}){$sample_index->{depositor_cellline_culture_conditions}{passage_method} = $$sample{depositor_cellline_culture_conditions}{passage_method};}
     if ($$sample{depositor_cellline_culture_conditions}{feeder_cell_type}){$sample_index->{depositor_cellline_culture_conditions}{feeder_cell_type} = $$sample{depositor_cellline_culture_conditions}{feeder_cell_type};}
-    if ($$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{amount}){$sample_index->{depositor_cellline_culture_conditions}{culture_medium_supplements}{amount} = $$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{amount};}
-    if ($$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{supplement}){$sample_index->{depositor_cellline_culture_conditions}{culture_medium_supplements}{supplement} = $$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{supplement};}
-    if ($$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{unit}){$sample_index->{depositor_cellline_culture_conditions}{culture_medium_supplements}{unit} = $$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}{unit};}
-    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage_flag}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage_flag} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage_flag};}
-    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo_flag}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo_flag} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo_flag};}
-    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw_flag}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw_flag} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw_flag};}
+    if ($$sample{depositor_cellline_culture_conditions}{culture_medium_supplements}){$sample_index->{depositor_cellline_culture_conditions}{culture_medium_supplements} = $$sample{depositor_cellline_culture_conditions}{culture_medium_supplements};}
+    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_passage};}
+    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_cryo};}
+    if ($$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw}){$sample_index->{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw} = $$sample{depositor_cellline_culture_conditions}{rock_inhibitor_used_at_thaw};}
+
+    if ($$sample{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag}){$sample_index->{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag} = $$sample{cellline_disease_associated_genotype}{carries_disease_phenotype_associated_variants_flag};}  #TODO No data yet needs to be tested when data is availible
+    if ($$sample{cellline_disease_associated_genotype}{variant_of_interest_flag}){$sample_index->{cellline_disease_associated_genotype}{variant_of_interest_flag} = $$sample{cellline_disease_associated_genotype}{variant_of_interest_flag};}  #TODO No data yet needs to be tested when data is availible
+
+    if ($$sample{cellline_certificate_of_analysis}{certificate_of_analysis_flag}){$sample_index->{cellline_certificate_of_analysis}{certificate_of_analysis_flag} = $$sample{cellline_certificate_of_analysis}{certificate_of_analysis_flag};}
 
     if ($$sample{virology_screening}{virology_screening_flag}){$sample_index->{virology_screening}{virology_screening_flag} = $$sample{virology_screening}{virology_screening_flag};}
     if ($$sample{virology_screening}{hiv1}){$sample_index->{virology_screening}{hiv1} = $$sample{virology_screening}{hiv1};}
@@ -106,9 +106,9 @@ sub subset_lims_fields {
     if ($$sample{virology_screening}{hepatitis_b}){$sample_index->{virology_screening}{hepatitis_b} = $$sample{virology_screening}{hepatitis_b};}
     if ($$sample{virology_screening}{hepatitis_c}){$sample_index->{virology_screening}{hepatitis_c} = $$sample{virology_screening}{hepatitis_c};}
 
-    if ($$sample{cellline_certificate_of_analysis}{certificate_of_analysis_flag}){$sample_index->{cellline_certificate_of_analysis}{certificate_of_analysis_flag} = $$sample{cellline_certificate_of_analysis}{certificate_of_analysis_flag};}
-
-    #TODO Charachterisation data
+    #TODO Uncomment and test when implemented at hPSCreg
+    #TODO Charachterisation fields
+    #TODO Genetic modification fields
 
     push(@filtered, $sample_index);
   }
