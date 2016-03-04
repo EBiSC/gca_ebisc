99b625f9067402ac20db5b3107aca9bcac3cdaf2
peterwharrison <peter@ebi.ac.uk>
2016-03-04 10:53:16 +0000

    Remove elements that are no longer needed

--- tracking_code/scripts/create_LIMS_import_xml.pl
+++ tracking_code/scripts/create_LIMS_import_xml.pl
@@ -8,7 +8,7 @@ use ReseqTrack::EBiSC::IMS;
 use ReseqTrack::EBiSC::XMLUtils qw(dump_xml);
 use ReseqTrack::EBiSC::BioSampleUtils;
 
-my ($IMS_user, $IMS_pass, $json_filename_in);
+my ($IMS_user, $IMS_pass);
 
 GetOptions("ims_user=s" => \$IMS_user,
     "ims_pass=s" => \$IMS_pass,
