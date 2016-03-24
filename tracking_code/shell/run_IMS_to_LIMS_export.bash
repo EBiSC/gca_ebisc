1687548bf8bda90a850844a4762781b9195e3d53
peterwharrison <peter@ebi.ac.uk>
2016-03-24 10:30:16 +0000

    Create shell script for IMS export to LIMS

---
+++ tracking_code/shell/run_IMS_to_LIMS_export.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+VERSION=`date "+%Y%m%d.%H%M%S"`
+OUTGOING_FTP=/nfs/production/reseq-info/drop/ebisc-data/outgoing
+
+perl $EBISC_CODE/tracking_code/scripts/create_LIMS_import_xml.pl -IMS_user="$IMS_USER" -IMS_pass="$IMS_PASS" > $OUTGOING_FTP/IMS_depositor_exports/IMS_depositor_export_$VERSION.xml
