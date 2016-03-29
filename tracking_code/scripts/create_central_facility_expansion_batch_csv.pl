597b18d4a8ba9614124cfc38eaecb322f8a9548d
peterwharrison <peter@ebi.ac.uk>
2016-03-29 14:21:54 +0100

    Obtain all batch data from IMS

--- tracking_code/scripts/create_central_facility_expansion_batch_csv.pl
+++ tracking_code/scripts/create_central_facility_expansion_batch_csv.pl
@@ -1,4 +1,4 @@
-!/usr/bin/env perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
@@ -7,6 +7,7 @@ use JSON qw(decode_json);
 use ReseqTrack::EBiSC::IMS;
 use ReseqTrack::EBiSC::XMLUtils qw(dump_xml);
 use ReseqTrack::EBiSC::BioSampleUtils;
+use Data::Dumper;
 
 my ($IMS_user, $IMS_pass);
 
@@ -20,3 +21,28 @@ my $IMS = ReseqTrack::EBiSC::IMS->new(
   pass => $IMS_pass,
 );
 
+my @batches;
+foreach my $sample (@{$IMS->find_lines->{'objects'}}){
+  if (defined $$sample{batches}){
+    foreach my $batch (@{$sample->{batches}}){
+      my $csvbatch = {};
+      $csvbatch = {
+      depositors_name => join('; ', @{$sample->{alternative_names}}), #TODO Get correct delimter from Maja
+      hescreg_name => $sample->{name},
+      ecacc_number => $sample->{ecacc_cat_no},
+      name => $batch->{batch_id},
+      batch_id => $batch->{biosamples_id},
+      vial => [map { {vial_id => $_->{biosamples_id}, vial_number => $_->{number} }} @{$batch->{vials}}]
+      };
+      push(@batches, $csvbatch);
+    }
+  }
+}
+foreach my $batch (@batches){
+  #TODO Run checks on existing files?
+  #TODO Create csv output file
+  print "Depositors Cell Line Name,hESCreg  Name,ECACC Cat no,Batch,Biosamples Batch ID,Vial number,Biosamples Vial ID\n";
+  foreach my $vial (@{$batch->{vial}}) {
+    print join(',', $$batch{depositors_name}, $$batch{hescreg_name}, $$batch{ecacc_number}, $$batch{name}, $$batch{batch_id}, $$vial{vial_number}, $$vial{vial_id}), "\n";
+  }
+}
