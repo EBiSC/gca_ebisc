cf9ac5b1ca069de5901ffb83af9dfe8a48b49bd5
peterwharrison <peter@ebi.ac.uk>
2016-03-31 10:25:43 +0100

    Switch to production server add correct vial format

--- tracking_code/shell/run_IMS_to_LIMS_export.bash
+++ tracking_code/shell/run_IMS_to_LIMS_export.bash
@@ -2,4 +2,3 @@
 
 perl $EBISC_CODE/tracking_code/scripts/create_cellline_xml.pl -IMS_user="$IMS_USER" -IMS_pass="$IMS_PASS": \
 && perl $EBISC_CODE/tracking_code/scripts/create_batch_csv.pl -IMS_user="$IMS_USER" -IMS_pass="$IMS_PASS"
-
